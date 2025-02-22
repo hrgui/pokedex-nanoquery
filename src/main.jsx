4c339fe6976043fac33678c789d8ea1a2ecdf0e1
Harman Goei <5001432+hrgui@users.noreply.github.com>
2025-02-22 14:26:19 -0800

    fix: apparently i need abortcontroller now

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import "abortcontroller-polyfill";
 
 //const myStore = createStore();
 // const countAtom = atom(0);
--- src/svelte-main.js
+++ src/svelte-main.js
@@ -1,6 +1,7 @@
 import { mount } from "svelte";
 import "./index.css";
 import App from "./App.svelte";
+import "abortcontroller-polyfill";
 
 const app = mount(App, {
   target: document.getElementById("root"),
