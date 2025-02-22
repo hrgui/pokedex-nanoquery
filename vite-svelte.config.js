adf6981bc24465fe7ac2a171434613ce979ae176
Harman Goei <5001432+hrgui@users.noreply.github.com>
2025-02-22 14:23:16 -0800

    fix: what happens when we separate it

--- vite-svelte.config.js
+++ vite-svelte.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import legacy from "@vitejs/plugin-legacy";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 
@@ -10,7 +10,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   base: "./",
   plugins: [
-    react(),
+    svelte(),
     legacy({
       targets: ["defaults", "chrome >= 47"],
     }),
@@ -18,7 +18,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        react: resolve(__dirname, "index.html"),
+        svelte: resolve(__dirname, "svelte-index.html"),
       },
     },
   },
